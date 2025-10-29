232f6a0fd8269c7e310cbc6619bbdeca1e1860d4
benrich <benrichprince1@gmail.com>
2025-10-29 14:10:38 +0100

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,29 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Clients from "./pages/Clients";
 import Invoices from "./pages/Invoices";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div className="flex min-h-screen bg-gray-50">
+        {/* Sidebar stays fixed on the left */}
         <Sidebar />
-        <main className="flex-grow p-6">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/clients" element={<Clients />} />
-            <Route path="/invoices" element={<Invoices />} />
-          </Routes>
-        </main>
-        <footer/>
+
+        {/* Main area fills the rest of the screen */}
+        <div className="flex flex-col flex-1">
+          <main className="flex-grow p-6 bg-black">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/clients" element={<Clients />} />
+              <Route path="/invoices" element={<Invoices />} />
+            </Routes>
+          </main>
+
+          {/* Footer sticks to the bottom of the main area */}
+          <Footer />
+        </div>
       </div>
     </Router>
   );
